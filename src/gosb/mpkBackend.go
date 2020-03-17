378c6010c0dc4b4d12a85ee9742c5c9833579ee1
Ubuntu <ubuntu@ip-172-31-3-69.us-east-2.compute.internal>
2020-03-17 17:03:13 +0000

    mpkRegister & mpkTransfer

--- src/gosb/mpkBackend.go
+++ src/gosb/mpkBackend.go
@@ -8,47 +8,7 @@ package gosb
 import (
 	"errors"
 	"fmt"
-	"syscall"
 )
-
-// Pkey represents a protection key
-type Pkey int
-
-// PKRU represents a list of access rights to be stored in PKRU register
-type PKRU uint32
-
-// Prot represents a protection access right
-type Prot uint32
-
-// SysProt represents protection of the page table entries
-type SysProt int
-
-// Syscall number on x86_64
-const (
-	sysPkeyMprotect = 329
-	sysPkeyAlloc    = 330
-	sysPkeyFree     = 331
-)
-
-// Protections
-const (
-	ProtRWX Prot = 0b00
-	ProtRX  Prot = 0b10
-	ProtX   Prot = 0b11
-
-	SysProtRWX SysProt = syscall.PROT_READ | syscall.PROT_WRITE | syscall.PROT_EXEC
-	SysProtRX  SysProt = syscall.PROT_READ | syscall.PROT_EXEC
-	SysProtRW  SysProt = syscall.PROT_READ | syscall.PROT_WRITE
-	SysProtR   SysProt = syscall.PROT_READ
-)
-
-// AllRightsPKRU is the default value of the PKRU, that allows everything
-const AllRightsPKRU PKRU = 0
-
-// Mask
-const mask uint32 = 0xfffffff
-
-/** Higher Level Implementation **/
 
 var (
 	sandboxKeys map[SandId][]int
@@ -72,23 +32,18 @@ func getSectionWithoutAlloc() *Section{
 //If the section did not exist, it must be a dynamic library and hence should
 //be added to the package as such.
 func mpkRegister(id int, start, size uintptr) {
-	fmt.Println("/////////// Register ///////////")
-	fmt.Println(id)
 	pkg, ok := idToPkg[id]
 	if !ok {
-		fmt.Println("Package not found")
-		fmt.Println("///////////   Done   ///////////")
+		panic(errors.New("Package not found"))
 		return
 	}
 	
 	for _, section := range pkg.Sects {
 		if section.Addr == uint64(start) {
-			fmt.Println("Section found")
-			fmt.Println("///////////   Done   ///////////")
+			panic(errors.New("Section not found"))
 			return
 		}
 	}
-	fmt.Println("Adding new section")
 
 	// Pop a section from the free list
 	section := getSectionWithoutAlloc()
@@ -97,7 +52,6 @@ func mpkRegister(id int, start, size uintptr) {
 	// TODO(CharlyCst): Add Prot
 
 	pkg.Dynamic = append(pkg.Dynamic, section)
-	fmt.Println("///////////   Done   ///////////")
 }
 
 //TODO(charlyCst) implement this one.
@@ -105,11 +59,46 @@ func mpkRegister(id int, start, size uintptr) {
 //We need to transfer it from oldid to new id. Maybe fault if the oldid == newid or if we have an invalid id.
 //The same should apply for the previous function.
 func mpkTransfer(oldid, newid int, start, size uintptr) {
-	fmt.Println("/////////// Transfer ///////////")
-	fmt.Println("///////////   Done   ///////////")
+	// Sanity check
+	if oldid == newid {
+		panic(errors.New("Transfer a section from one package to itself"))
+	}
+	oldPkg, ok := idToPkg[oldid]
+	if !ok {
+		panic(errors.New("Old package not found"))
+	}
+	newPkg, ok := idToPkg[newid]
+	if !ok {
+		panic(errors.New("New package not found"))
+	}
+
+	// Find in old mapping, linear scan
+	found := false
+	var idx int
+	for i, section := range oldPkg.Dynamic {
+		if section.Addr == uint64(start) && section.Size == uint64(size) {
+			found = true
+			idx = i
+			fmt.Println("Found")
+			break
+		}
+	}
+	if !found {
+		panic(errors.New("Section not found"))
+	}
+
+	section := oldPkg.Dynamic[idx]
+
+	// Removing from old mapping
+	n := len(oldPkg.Dynamic)-1
+	oldPkg.Dynamic[idx] = oldPkg.Dynamic[n]
+	oldPkg.Dynamic = oldPkg.Dynamic[:n]
+
+	// Add to new mapping
+	newPkg.Dynamic[len(newPkg.Dynamic)] = section
 }
 
-// mpkInit relies on sandboxes and pkgToId, they must be initialized before the call
+// mpkInit relies on domains and packages, they must be initialized before the call
 func mpkInit() {
 	n := len(packages)
 	pkgAppearsIn := make(map[int][]SandId, n)
@@ -163,18 +152,6 @@ func mpkInit() {
 
 	// We have an allocation for the keys!
 	sandboxKeys = sbKeys
-	/* //TODO(aghosn) remove afterwards, it's just the allocation
-	for k, v := range sandboxKeys {
-		fmt.Println(k, "->", v)
-	}
-	fmt.Println("Groups")
-	for i, v := range pkgGroups {
-		fmt.Println(i, "->", v)
-	}
-	fmt.Println("Packages")
-	for _, p := range packages {
-		fmt.Println(p.Name, "->", p.Id)
-	}*/
 
 	fmt.Println("Sandbox keys:",sandboxKeys)
 	fmt.Println("Package groups:",pkgGroups)
@@ -202,54 +179,4 @@ func popMap(m map[int][]SandId) (int, []SandId) {
 		return id, group
 	}
 	return -1, nil
-}
-
-/** Low Level Implementation **/
-
-// WritePKRU updates the value of the PKRU
-func WritePKRU(prot PKRU)
-
-// ReadPKRU returns the value of the PKRU
-func ReadPKRU() PKRU
-
-func (p PKRU) String() string {
-	return fmt.Sprintf("0b%032b", p)
-}
-
-// Update returns a new PKRU with updated rights
-func (p PKRU) Update(pkey Pkey, prot Prot) PKRU {
-	pkeyMask := mask - (1 << (2 * pkey)) - (1 << (2*pkey + 1))
-	pkru := uint32(p) & pkeyMask
-	pkru += uint32(prot) << (2 * pkey)
-
-	return PKRU(pkru)
-}
-
-// PkeyAlloc allocates a new pkey
-func PkeyAlloc() (Pkey, error) {
-	pkey, _, _ := syscall.Syscall(sysPkeyAlloc, 0, 0, 0)
-	if int(pkey) < 0 {
-		return Pkey(pkey), errors.New("Failled to allocate pkey")
-	}
-	return Pkey(pkey), nil
-}
-
-// PkeyFree frees a pkey previously allocated
-func PkeyFree(pkey Pkey) error {
-	result, _, _ := syscall.Syscall(sysPkeyFree, uintptr(pkey), 0, 0)
-	if result != 0 {
-		return errors.New("Could not free pkey")
-	}
-	return nil
-}
-
-// PkeyMprotect tags pages within [addr, addr + len -1] with the given pkey.
-// Permission on page table can also be update at the same time.
-// Note that addr must be aligned to a page boundary.
-func PkeyMprotect(addr uintptr, len uint64, sysProt SysProt, pkey Pkey) error {
-	result, _, _ := syscall.Syscall6(sysPkeyMprotect, addr, uintptr(len), uintptr(sysProt), uintptr(pkey), 0, 0)
-	if result != 0 {
-		return errors.New("Could not update memory access rights")
-	}
-	return nil
 }
